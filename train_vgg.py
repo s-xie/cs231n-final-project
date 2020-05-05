0cc54747b2c642009788fa682ad2a6a8a43f1aaa
Nicholas Benavides <12nbenavides@gmail.com>
2020-05-04 22:14:50 -0400

    added callbacks, fixed plotting, and decomposed loading dataset

--- train_vgg.py
+++ train_vgg.py
@@ -1,24 +1,20 @@
+# Imports
 import tensorflow as tf
 from tensorflow.keras import layers, models, optimizers, losses
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.metrics import TopKCategoricalAccuracy, Accuracy
-import tensorflow_docs as tfdocs
-import tensorflow_docs.plots
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+from time import gmtime, strftime
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-# will remove once we have actual data
-import tensorflow_datasets as tfds 
-tfds.disable_progress_bar()
-
-# # Construct a tf.data.Dataset
-# dataset, info = tfds.load(name="cifar10", as_supervised = True, with_info = True)
-# train_dataset, test_dataset = dataset['train'], dataset['test']
-# num_train_examples = info.splits['train'].num_examples
-
+# Constants & Parameters
+DATA_DIR = ""
+OUTPUT_DIR = "models/"
 IMG_SIZE = 224
 IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
 BATCH_SIZE = 25
@@ -27,7 +23,8 @@ N_CLASSES = 10 # 101 for actual training
 N_LAYERS_TO_FREEZE = 17 # freeze everything before the last conv layer
 lr = 1e-4
 
-def load_cfar10_batch(cifar10_dataset_folder_path, batch_id):
+# Load Training & Validation Data
+def load_cfar10_batch(batch_id):
 	with open('../cifar-10-batches-py' + '/data_batch_' + str(batch_id), mode='rb') as file:
 		# note the encoding type is 'latin1'
 		batch = pickle.load(file, encoding='latin1')
@@ -43,35 +40,38 @@ def one_hot_encode(x):
 		encoded[idx][val] = 1
 	return encoded
 
-train_features = None
-train_labels = None
-for batch_i in range(1, 6):
-	features, labels = load_cfar10_batch('../cifar-10-batches-py', batch_i)
-	encoded_labels = one_hot_encode(labels)
-	if batch_i == 1:
-		train_features = features 
-		train_labels = encoded_labels
-	else:
-		train_features = np.concatenate((train_features, features), axis = 0)
-		#print(train_labels.shape)
-		#print(labels.shape)
-		train_labels = np.concatenate((train_labels, encoded_labels), axis = 0)
-print(train_features.shape)
-print(train_labels.shape)
-num_train_examples = train_features.shape[0]
+def get_cfar10_local():
+	train_features = None
+	train_labels = None
+	for batch_i in range(1, 6):
+		features, labels = load_cfar10_batch(batch_i)
+		encoded_labels = one_hot_encode(labels)
+		if batch_i == 1:
+			train_features = features 
+			train_labels = encoded_labels
+		else:
+			train_features = np.concatenate((train_features, features), axis = 0)
+			train_labels = np.concatenate((train_labels, encoded_labels), axis = 0)
+	print(train_features.shape)
+	print(train_labels.shape)
+	num_train_examples = train_features.shape[0]
 
-test_features, test_labels = None, None
-with open('../cifar-10-batches-py' + '/test_batch', mode='rb') as file:
-	batch = pickle.load(file, encoding='latin1')
+	test_features, test_labels = None, None
+	with open('../cifar-10-batches-py' + '/test_batch', mode='rb') as file:
+		batch = pickle.load(file, encoding='latin1')
 
-	# preprocess the testing data
-	test_features = batch['data'].reshape((len(batch['data']), 3, 32, 32)).transpose(0, 2, 3, 1)
-	test_labels = batch['labels']
-	test_labels = one_hot_encode(test_labels)
+		# preprocess the testing data
+		test_features = batch['data'].reshape((len(batch['data']), 3, 32, 32)).transpose(0, 2, 3, 1)
+		test_labels = batch['labels']
+		test_labels = one_hot_encode(test_labels)
 
-train_dataset = tf.data.Dataset.from_tensor_slices((train_features, train_labels))
-test_dataset = tf.data.Dataset.from_tensor_slices((test_features, test_labels))
+	train_dataset = tf.data.Dataset.from_tensor_slices((train_features, train_labels))
+	test_dataset = tf.data.Dataset.from_tensor_slices((test_features, test_labels))
+	return train_dataset, test_dataset, num_train_examples
 
+train_dataset, test_dataset, num_train_examples = get_cfar10_local()
+
+# Image Normalization, Resizing, and Augmentation
 # modified from https://www.tensorflow.org/tutorials/images/data_augmentation
 def convert(image, label):
 	image = tf.image.resize(image, size = [IMG_SIZE, IMG_SIZE])
@@ -80,13 +80,13 @@ def convert(image, label):
 
 def augment(image,label):
 	image,label = convert(image, label)
-	image = tf.image.convert_image_dtype(image, tf.float32) # Cast and normalize the image to [0,1]
 	image = tf.image.random_flip_left_right(image)
 	image = tf.image.resize_with_crop_or_pad(image, IMG_SIZE + 16, IMG_SIZE + 16) # Add 16 pixels of padding
 	image = tf.image.random_crop(image, size=[IMG_SIZE, IMG_SIZE, 3]) # Random crop back to 224x224
 	image = tf.image.random_brightness(image, max_delta=0.25) # Random brightness
 	return image,label
 
+# Generate batches at tf.data.Dataset objects
 augmented_train_batches = (
 		train_dataset
 		.take(NUM_EXAMPLES) # change to -1 to get full dataset for actual training
@@ -97,10 +97,6 @@ augmented_train_batches = (
 		.prefetch(AUTOTUNE)
 )
 
-# for element in augmented_train_batches.as_numpy_iterator():
-# 	print(element) 
-# 	assert False
-
 validation_batches = (
 		test_dataset
 		.take(NUM_EXAMPLES) # change to -1 to get full dataset for actual training
@@ -108,6 +104,7 @@ validation_batches = (
 		.batch(BATCH_SIZE)
 )
 
+# Define and load model
 # borrowed from https://github.com/nnbenavides/Fine-Grained-Vehicle-Classification/blob/master/train_vgg_model.py
 def freeze_layers(model):
 	print(len(model.layers))
@@ -136,6 +133,23 @@ def get_model():
 	print(model.summary()) # 5 conv blocks + 2 FC layers, 27.6M total params, 15.2M trainable params
 	return model
 
+# Make sure we have an output folder for model files, history, and plots
+if not os.path.exists(OUTPUT_DIR):
+	os.mkdir(OUTPUT_DIR)
+model_folder = os.path.join(OUTPUT_DIR, 'model_' + str(strftime("%Y-%m-%d_%H:%M:%S", gmtime())))
+if not os.path.exists(model_folder):
+	os.mkdir(model_folder)
+plot_folder = os.path.join(model_folder, 'plots/')
+if not os.path.exists(plot_folder):
+	os.mkdir(plot_folder)
+
+# Define callbacks
+checkpoint = ModelCheckpoint(filepath = os.path.join(model_folder, 'model.hdf5'), save_best_only = True,
+							monitor = 'val_accuracy', save_weights_only = False, verbose = 0)
+early_stop = EarlyStopping(monitor = 'val_accuracy', patience = 10)
+#tb = TensorBoard(log_dir = LOG_DIR, batch_size = BATCH_SIZE)
+
+# Compile & train model
 model = get_model()
 adam = optimizers.Adam(learning_rate = lr, beta_1 = 0.9, beta_2 = 0.99, epsilon = None, decay = 1e-5, amsgrad = False)
 model.compile(loss = 'categorical_crossentropy',
@@ -143,19 +157,21 @@ model.compile(loss = 'categorical_crossentropy',
 							metrics = [Accuracy(), TopKCategoricalAccuracy(k = 3, name = 'top3_accuracy'), TopKCategoricalAccuracy(k = 5, name = 'top5_accuracy')])
 print('Model compiled')
 
-model_history = model.fit(augmented_train_batches, epochs = 5, validation_data = validation_batches)
+model_history = model.fit(augmented_train_batches, epochs = 5, validation_data = validation_batches, callbacks = [checkpoint, early_stop])
 
-plotter = tfdocs.plots.HistoryPlotter()
-plotter.plot({'Model':model_history}, metric = "accuracy")
-plt.title("Accuracy")
-plt.show()
-
-plotter = tfdocs.plots.HistoryPlotter()
-plotter.plot({'Model':model_history}, metric = "top3_accuracy")
-plt.title("Top 3 Accuracy")
-plt.show()
-
-plotter = tfdocs.plots.HistoryPlotter()
-plotter.plot({'Model':model_history}, metric = "loss")
-plt.title("Loss")
-plt.show()
+# Save output
+np.save(os.path.join(model_folder, 'history.npy'), model_history.history)
+metrics = ['loss', 'accuracy', 'top3_accuracy', 'top5_accuracy']
+titles = ['Loss', 'Accuracy', 'Top 3 Accuracy', 'Top 5 Accuracy']
+for i, metric in enumerate(metrics):
+	train_metric = model_history.history[metric]
+	val_metric = model_history.history['val_' + metric]
+	plt.figure()
+	epochs = range(len(train_metric))
+	plt.plot(epochs, train_metric, label = 'Train')
+	plt.plot(epochs, val_metric, label = 'Validation')
+	plt.title(titles[i] + ' Over Time')
+	plt.xlabel('Epochs')
+	plt.ylabel(titles[i])
+	plt.legend()
+	plt.savefig(os.path.join(plot_folder, metric + '.png'), dpi = 200)
